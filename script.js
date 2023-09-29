a7aa3be2692f8c57461fa0a34eb3047ccd4f0c9b
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-29 13:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let strong = document.getElementsByTagName("strong")
+
 function highlight() {
     //Write your code here
-
-
+    for (const element of strong) {
+        element.style.color = "green";
+      }
+ 
 }
-
 
 function return_normal() {
     //Write your code here
 
-    
+    for (const element of strong) {
+        element.style.color = "black";
+      }
 }
